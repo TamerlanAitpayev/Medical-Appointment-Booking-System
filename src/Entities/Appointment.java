b33b42823a9d5c2899f1d97aca7f64a56ee7857c
unknown <251274@gmail.com>
2026-01-28 11:36:23 +0500

    I added db

--- src/Entities/Appointment.java
+++ src/Entities/Appointment.java
@@ -1,4 +1,37 @@
 package Entities;
 
+import java.sql.Timestamp;
+
 public class Appointment {
+    private int id;
+    private int patientId;
+    private int doctorId;
+    private Timestamp appointmentTime;
+    private String status;
+
+    public Appointment() {}
+
+    public Appointment(int id, int patientId, int doctorId, String status, Timestamp appointmentTime) {
+        this.id = id;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.status = status;
+        this.appointmentTime = appointmentTime;
+    }
+
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getPatientId() { return patientId; }
+    public void setPatientId(int patientId) { this.patientId = patientId; }
+
+    public int getDoctorId() { return doctorId; }
+    public void setDoctorId(int doctorId) { this.doctorId = doctorId; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public Timestamp getAppointmentTime() { return appointmentTime; }
+    public void setAppointmentTime(Timestamp timestamp) { this.appointmentTime = timestamp; }
 }
